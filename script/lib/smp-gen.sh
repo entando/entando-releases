a2d4bb3aa0a0f76a32622ce3125aeb5eb8c34d80
pietrangelo <p.masala@entando.com>
2023-07-26 12:23:05 +0200

    IT-421: copy kustomization files to samples folder

--- script/lib/smp-gen.sh
+++ script/lib/smp-gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd "$DIR/../.."
 
 . ./manifest
@@ -18,10 +18,10 @@ _set_catalog_placeholders() {
   local CATALOG_NAME="entando-${APPVER//./-}"
   local CATALOG_DISP_NAME="Entando $APPVER"
 
-  cat - \
-    | sed "s/{{NAME}}/$CATALOG_NAME/g" \
-    | sed "s/{{DISPLAY-NAME}}/\"$CATALOG_DISP_NAME\"/g" \
-    | sed "s|{{IMAGE}}|$OKD_CATALOG_IMAGE|g" \
+  cat - |
+    sed "s/{{NAME}}/$CATALOG_NAME/g" |
+    sed "s/{{DISPLAY-NAME}}/\"$CATALOG_DISP_NAME\"/g" |
+    sed "s|{{IMAGE}}|$OKD_CATALOG_IMAGE|g" \
     ;
 }
 
@@ -36,30 +36,32 @@ _set_placeholders() {
   local CATALOG_NAME="entando-catalog-${APPVER//./-}"
   local CATALOG_DISP_NAME="Entando $APPVER"
 
-  cat - \
-    | sed "s/{{ENTANDO_NAMESPACE}}/$ENTANDO_NAMESPACE/" \
-    | sed "s/{{ENTANDO_APP_NAME}}/$ENTANDO_APPNAME/" \
-    | sed "s/{{ENTANDO_HOSTNAME}}/$ENTANDO_HOSTNAME/" \
-    | sed "s/{{ENTANDO_APP_IMAGE_TYPE}}/$IMGTYPE/" \
-    | sed "s/{{ENTANDO_APP_REPLICAS}}/$REPLICA/" \
-    | sed "s/{{ENTANDO_DBMS}}/$DB/" \
-    | sed "s/{{ENTANDO_APP_VERSION}}/\"$APPVER\"/" \
-    | sed "s/{{ENTANDO_VARS}}/null/" \
+  cat - |
+    sed "s/{{ENTANDO_NAMESPACE}}/$ENTANDO_NAMESPACE/" |
+    sed "s/{{ENTANDO_APP_NAME}}/$ENTANDO_APPNAME/" |
+    sed "s/{{ENTANDO_HOSTNAME}}/$ENTANDO_HOSTNAME/" |
+    sed "s/{{ENTANDO_APP_IMAGE_TYPE}}/$IMGTYPE/" |
+    sed "s/{{ENTANDO_APP_REPLICAS}}/$REPLICA/" |
+    sed "s/{{ENTANDO_DBMS}}/$DB/" |
+    sed "s/{{ENTANDO_APP_VERSION}}/\"$APPVER\"/" |
+    sed "s/{{ENTANDO_VARS}}/null/" \
     ;
 }
 
 mkdir -p "$SMPD"
 
-cat "$TPLD/base/entando-app.yaml" | _set_placeholders > "$SMPD/entando-app.yaml"
-cat "$TPLD/base/entando-operator-config.yaml" | _set_placeholders > "$SMPD/entando-operator-config.yaml"
-cat "$TPLD/base/entando-operator-config.yaml" | _set_placeholders > "$SMPD/entando-operator-config.yaml"
-cat "$TPLD/base/entando-solrCloud.yaml" | _set_placeholders > "$SMPD/entando-solrCloud.yaml"
-cat "$TPLD/base/entando-tenant-ingress.yaml" | _set_placeholders > "$SMPD/entando-tenant-ingress.yaml"
-cat "$TPLD/base/entando-tenants-secret.yaml" | _set_placeholders > "$SMPD/entando-tenants-secret.yaml"
+cat "$TPLD/base/entando-app.yaml" | _set_placeholders >"$SMPD/entando-app.yaml"
+cat "$TPLD/base/entando-operator-config.yaml" | _set_placeholders >"$SMPD/entando-operator-config.yaml"
+cat "$TPLD/base/entando-operator-config.yaml" | _set_placeholders >"$SMPD/entando-operator-config.yaml"
+cat "$TPLD/base/entando-solrCloud.yaml" | _set_placeholders >"$SMPD/entando-solrCloud.yaml"
+cat "$TPLD/base/entando-tenant-ingress.yaml" | _set_placeholders >"$SMPD/entando-tenant-ingress.yaml"
+cat "$TPLD/base/entando-tenants-secret.yaml" | _set_placeholders >"$SMPD/entando-tenants-secret.yaml"
 
 # OTHERS
-cat "$TPLD/misc/catalog-source.yaml" | _set_catalog_placeholders > "$SMPD/openshift-catalog-source.yaml"
-cat "$TPLD/misc/entando-solrCloud.yaml" > "$SMPD/entando-solrCloud.yaml"
-cat "$TPLD/misc/entando-tenant-ingress.yaml" > "$SMPD/entando-tenant-ingress.yaml"
-cat "$TPLD/misc/entando-tenants-secret.yaml" > "$SMPD/entando-tenants-secret.yaml"
-cat "$TPLD/misc/entando-cds.yaml" > "$SMPD/entando-cds.yaml"
+cat "$TPLD/misc/catalog-source.yaml" | _set_catalog_placeholders >"$SMPD/openshift-catalog-source.yaml"
+cat "$TPLD/misc/entando-solrCloud.yaml" >"$SMPD/entando-solrCloud.yaml"
+cat "$TPLD/misc/entando-tenant-ingress.yaml" >"$SMPD/entando-tenant-ingress.yaml"
+cat "$TPLD/misc/entando-tenants-secret.yaml" >"$SMPD/entando-tenants-secret.yaml"
+cat "$TPLD/misc/entando-cds.yaml" >"$SMPD/entando-cds.yaml"
+cat "$TPLD/misc/kustomization-K8S.yaml" >"$SMPD/kustomization-K8S.yaml"
+cat "$TPLD/misc/kustomization-OCP.yaml" >"$SMPD/kustomization-OCP.yam"
